70ab38188d422ca227d56132acb85a687f8625cd
Vitória Pizzutti <pizzutti.v@gmail.com>
2024-06-06 11:39:01 -0300

    first commit

---
+++ src/main/java/com/elc1090/shelterhubapi/ShelterhubApiApplication.java
@@ -0,0 +1,13 @@
+package com.elc1090.shelterhubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterhubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelterhubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elc1090/shelterhubapi/ShelterhubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elc1090.shelterhubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterhubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
